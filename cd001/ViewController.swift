c65072315173a821607b1b89aa0bfed7eb53f621
robita34 <dxwnh157@gmail.com>
2014-11-03 16:45:53 +0900

    Initial Commit

---
+++ cd001/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cd001
+//
+//  Created by arith11 on 2014/11/03.
+//  Copyright (c) 2014年 arith11. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ cd001Tests/cd001Tests.swift
@@ -0,0 +1,36 @@
+//
+//  cd001Tests.swift
+//  cd001Tests
+//
+//  Created by arith11 on 2014/11/03.
+//  Copyright (c) 2014年 arith11. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class cd001Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
